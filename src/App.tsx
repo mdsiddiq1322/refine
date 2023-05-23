3bf11b3534416ada1d9248e0bf621a182bb4f323
Mohamed Siddiq <mdsiddiq1322@gmail.com>
2023-05-23 23:58:14 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import { ErrorComponent, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import { notificationProvider, RefineThemes } from "@refinedev/mantine";
+import { notificationProvider, RefineThemes, ThemedLayoutV2 } from "@refinedev/mantine";
 import { MantineInferencer } from "@refinedev/inferencer/mantine";
 
 import {
@@ -13,10 +13,11 @@ import {
 import { useLocalStorage } from "@mantine/hooks";
 import { NotificationsProvider } from "@mantine/notifications";
 import routerBindings, {
+  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import dataProvider from "@refinedev/simple-rest";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
@@ -29,42 +30,49 @@ function App() {
     setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
   return (
-    <BrowserRouter>
-      <GitHubBanner />
-      <RefineKbarProvider>
-        <ColorSchemeProvider
-          colorScheme={colorScheme}
-          toggleColorScheme={toggleColorScheme}
-        >
-          {/* You can change the theme colors here. example: theme={{ ...RefineThemes.Magenta, colorScheme:colorScheme }} */}
-          <MantineProvider
-            theme={{ ...RefineThemes.Blue, colorScheme: colorScheme }}
+    <MantineProvider
+            theme={RefineThemes.Blue}
             withNormalizeCSS
             withGlobalStyles
-          >
+        >
             <Global styles={{ body: { WebkitFontSmoothing: "auto" } }} />
             <NotificationsProvider position="top-right">
-              <Refine
-                notificationProvider={notificationProvider}
-                routerProvider={routerBindings}
-                dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
-                options={{
-                  syncWithLocation: true,
-                  warnWhenUnsavedChanges: true,
-                }}
-                resources={[
-                  {
-                      name: "blog_posts",
-                      list: "/blog-posts",
-                      show: "/blog-posts/show/:id",
-                      create: "/blog-posts/create",
-                      edit: "/blog-posts/edit/:id",
-                  },
-              ]}
-              >
-                <Routes>
-                  <Route index element={<WelcomePage />} />
-                  <Route path="blog-posts">
+                <BrowserRouter>
+                    <Refine
+                        routerProvider={routerBindings}
+                        dataProvider={dataProvider(
+                            "https://api.fake-rest.refine.dev",
+                        )}
+                        notificationProvider={notificationProvider}
+                        resources={[
+                            {
+                                name: "blog_posts",
+                                list: "/blog-posts",
+                                show: "/blog-posts/show/:id",
+                                create: "/blog-posts/create",
+                                edit: "/blog-posts/edit/:id",
+                            },
+                        ]}
+                        options={{
+                            syncWithLocation: true,
+                            warnWhenUnsavedChanges: true,
+                        }}
+                    >
+                        <Routes>
+                            <Route
+                                element={
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                }
+                            >
+                                <Route
+                                    index
+                                    element={
+                                        <NavigateToResource resource="blog_posts" />
+                                    }
+                                />
+                                <Route path="blog-posts">
                                     <Route
                                         index
                                         element={<MantineInferencer />}
@@ -82,15 +90,14 @@ function App() {
                                         element={<MantineInferencer />}
                                     />
                                 </Route>
-                </Routes>
-                <RefineKbar />
-                <UnsavedChangesNotifier />
-              </Refine>
+                                <Route path="*" element={<ErrorComponent />} />
+                            </Route>
+                        </Routes>
+                        <UnsavedChangesNotifier />
+                    </Refine>
+                </BrowserRouter>
             </NotificationsProvider>
-          </MantineProvider>
-        </ColorSchemeProvider>
-      </RefineKbarProvider>
-    </BrowserRouter>
+        </MantineProvider>
   );
 }
 
